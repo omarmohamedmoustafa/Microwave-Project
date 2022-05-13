b17f578efb2ce54971a7c576b896c5f1d54760e5
mohamdhassan0x7 <105478873+mohamdhassan0x7@users.noreply.github.com>
2022-05-13 09:24:32 +0200

    Add files via upload

---
+++ Lcd.h
@@ -0,0 +1,8 @@
+void PortA_Init(void);
+void PortB_Init(void);
+
+void lcd_command2(unsigned char cmd);
+void lcd_init2(void);
+void lcdout( unsigned char value);
+void lcdprint (char *str);
+void lcdoutNum (int number);
